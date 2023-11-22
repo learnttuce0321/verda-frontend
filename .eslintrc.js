450f711f4be58f491859efbeb47a738807898fd0
learnttuce0321 <lettucegood2@gmail.com>
2023-11-22 16:44:44 +0900

    Set: eselintrc 파일 변경

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    createDefaultProgram: "true",
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals",
+    // "@typescript-eslint/parser",
+  ],
+  rules: {
+    // 'React' must be in scope when using JSX 에러 해결 (Next.js)
+    "react/react-in-jsx-scope": "off",
+    // ts파일에서 tsx구문 허용 (Next.js)
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+  },
+};
