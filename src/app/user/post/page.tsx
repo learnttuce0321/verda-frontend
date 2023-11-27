859b5e62170589903f04f8d08e4f95510c27060a
learnttuce0321 <lettucegood2@gmail.com>
2023-11-27 13:00:33 +0900

    Feat: 무한 스크롤 제외 데이터 페칭

--- src/app/user/post/page.tsx
+++ src/app/user/post/page.tsx
@@ -1,22 +1,6 @@
-"use client";
-
-import Link from "next/link";
 import Section from "@/components-kim/Section";
 import TextStore, { TextStyle } from "@/Components/Atom/Text/TextStore";
-import ButtonListTitle from "@/Components/Molecure/Button-jsh/List/ButtonListTitle";
-
-const POST_DUMMYDATA = [
-  {
-    id: 1,
-    fundManagerName: "주상후",
-    title: "미국 주식 투자 문의",
-  },
-  {
-    id: 2,
-    fundManagerName: "손영석",
-    title: "미국 펀드 투자 문의",
-  },
-];
+import PostList from "@/Components/Organism/Post/PostList";
 
 export default function PostListPage() {
   return (
@@ -26,17 +10,8 @@ export default function PostListPage() {
           내 문의글
         </TextStore>
       </Section>
-      <section>
-        {POST_DUMMYDATA.map(post => {
-          return (
-            <Link href={`/user/post/${post.id}`} key={post.id}>
-              <ButtonListTitle>
-                {post.title}
-              </ButtonListTitle>
-            </Link>
-          )
-        })}
-      </section>
+
+      <PostList />
     </>
   );
 }
--- src/app/user/rooms/[roomId]/page.tsx
+++ src/app/user/rooms/[roomId]/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { notFound } from "next/navigation";
 import BoxStore, { BoxStyle } from "@/Components/Atom/Box/BoxStore";
 import TextStore, { TextStyle } from "@/Components/Atom/Text/TextStore";
-import InputMessage from "@/Components/Molecure/input-kmr/InputMessage";
 
 interface Props {
   params: {
@@ -11,135 +8,26 @@ interface Props {
   };
 };
 
-const CHATLIST_DUMMYDATA = [
-  {
-    id: 1,
-    fundManagerName: '주상후',
-    firstMessage:
-      '안녕하세요. 저는 펀드매니저 주상후 입니다. 이번건에 대허서 답장을 드리기 위해 연락을 드렸습니다.',
-    secondMessage: '안녕하세요',
-  },
-  {
-    id: 2,
-    fundManagerName: '손영석',
-    firstMessage:
-      '안녕하세요. 저는 펀드매니저 손영석 입니다. 이번건에 대허서 답장을 드리기 위해 연락을 드렸습니다.',
-    secondMessage: '하',
-  },
-  {
-    id: 3,
-    fundManagerName: '김미리',
-    firstMessage:
-      '안녕하세요. 저는 펀드매니저 김미리 입니다. 이번건에 대허서 답장을 드리기 위해 연락을 드렸습니다.',
-    secondMessage: 'ㅁ유햐',
-  },
-];
+export default async function ChatDetailPage({ params: { roomId } }: Props) {
+  const getChat = async (): Promise<any> => {
+    const res = await fetch(`${process.env.BASE_URL}/api/chat/${roomId}`, {
+      method: "GET",
+    });
 
-export default function ChatDetailPage({ params: { roomId } }: Props) {
-  const data = CHATLIST_DUMMYDATA.find(chat => chat.id === parseInt(roomId, 10));
-
-  if (data === undefined) {
-    notFound();
+    return res.json()
   }
+  const { preChatList: chats } = await getChat()
+
   return (
     <section className="w-[100%] ">
       <TextStore textStyle={TextStyle.TEXT_R_40_BLUE} style="mb-2">
-        {data?.fundManagerName}
+        {/* {data?.fundManagerName} */}
       </TextStore>
       <div className="border-2 opacity-100 h-[calc(100vh_-_126px)] overflow-y-scroll">
         {/* todos : 이거 백엔드랑 연결 후 설정해야됨 */}
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_SMOG}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.firstMessage}
-          </TextStore>
-        </BoxStore>
-
-        <BoxStore boxStyle={BoxStyle.BOX_CHAT_ORANGE}>
-          <TextStore textStyle={TextStyle.TEXT_S_16}>
-            {data?.secondMessage}
-          </TextStore>
-        </BoxStore>
       </div>
       {/* </section> */}
       {/* todos: input 생성해야됨 */}
-      {/* <section> */}
     </section>
   );
 }
