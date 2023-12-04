0c627b2d276791a06b2afa8d1894696a286c2be0
learnttuce0321 <lettucegood2@gmail.com>
2023-12-04 10:09:43 +0900

    Feat: 네비게이션 바

--- src/app/signupUser/page.tsx
+++ src/app/signupUser/page.tsx
@@ -1,6 +1,10 @@
 import SignupUsers from "@/Component-son/SignupUser";
 
 function SignupUserRouter() {
-  return <SignupUsers></SignupUsers>;
+  return (
+    <div className="pt-[100px]">
+      <SignupUsers />
+    </div>
+  );
 }
 export default SignupUserRouter;
